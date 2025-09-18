875df38a3f8cbd29df55d58fcceeeb7ea1536979
Randal Vanheede <randal.vanheede@entityone.be>
2025-09-18 10:55:37 +0200

    Fix "warning is not a supported key"

--- modules/accounts/src/Plugin/MultisiteHelperPlugin/AccountSync.php
+++ modules/accounts/src/Plugin/MultisiteHelperPlugin/AccountSync.php
@@ -36,12 +36,13 @@ final class AccountSync extends MultisiteHelperPluginBase {
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
 
-    $form['warning'] = [
-      '#type' => 'html_tag',
-      '#tag' => 'strong',
-      '#value' => (string) $this->t("If an SSO solution is used, it's advised to disable any and all account synchronization and just let the SSO provider handle it."),
-      '#weight' => -10,
-    ];
+    // @todo fix: "is not a supported key"
+//    $form['warning'] = [
+//      '#type' => 'html_tag',
+//      '#tag' => 'strong',
+//      '#value' => (string) $this->t("If an SSO solution is used, it's advised to disable any and all account synchronization and just let the SSO provider handle it."),
+//      '#weight' => -10,
+//    ];
 
     $roles = array_map(static function (RoleInterface $role) {
       return $role->label();
